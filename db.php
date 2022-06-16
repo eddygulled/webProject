771a8a427d1a49274ba6b1c56a4d74a4370f3f81
eddygulled <edgargulay69@gmail.com>
2022-06-17 01:55:07 +0300

    db config restored

--- db.php
+++ db.php
@@ -1,14 +1,12 @@
 <?php
-$host = "sql311.epizy.com";
-$user = "epiz_31625976";
-$pwd = "3HkkVcxUzjVbP";
-$dbname = "epiz_31625976_university";
+$host = "localhost";
+$user = "root";
+$pwd = "localhost";
+$dbname = "university";
 $port = 3306;
 
 $con = new mysqli($host, $user, $pwd, $dbname, $port);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
   }
-
-session_start();
 ?>
